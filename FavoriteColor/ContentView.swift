f6c38f8555575e9458e4d7a88e71d3ae6d082ffa
Art Huang <art.huang@ui.com>
2022-03-30 12:55:16 +0800

    Initial Commit

---
+++ FavoriteColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteColor
+//
+//  Created by Art Huang on 2022/3/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavoriteColor/FavoriteColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteColorApp.swift
+//  FavoriteColor
+//
+//  Created by Art Huang on 2022/3/30.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
